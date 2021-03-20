f837fff5751fa8b71ab390ad61238746a7b456e6
Athziry-Sierra <64499581+Athziry-Sierra@users.noreply.github.com>
2021-03-20 15:21:35 -0600

    Actividad 8 prototipado

---
+++ Prototipo_AZSM/Prototipo_AZSM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo_AZSM
+//
+//  Created by user167237 on 3/20/21.
+//  Copyright © 2021 user167237. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
