f8866fd022f3e543d1e11abdc0a0e292c72631c0
Athziry-Sierra <64499581+Athziry-Sierra@users.noreply.github.com>
2021-04-09 15:29:13 -0500

    Actividad 10

---
+++ Act10_AZSM/Act10_AZSM/Act10_AZSM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Act10_AZSM
+//
+//  Created by user167237 on 4/9/21.
+//  Copyright © 2021 user167237. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
